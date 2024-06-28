90a459e0f616910358bb60f817c653310ad2a2c4
ngpong <pengpeng1997428@gmail.com>
2024-06-29 00:28:35 +0800

    ...

--- .config/nvim/lua/ngpong/plugins/autoclose/config.lua
+++ .config/nvim/lua/ngpong/plugins/autoclose/config.lua
@@ -7,7 +7,7 @@ M.setup = function()
       ['['] = { escape = false, close = true, pair = '[]' },
       ['{'] = { escape = false, close = true, pair = '{}' },
 
-      ['>'] = { escape = true, close = false, pair = '<>' },
+      -- ['>'] = { escape = true, close = false, pair = '<>' },
       [')'] = { escape = true, close = false, pair = '()' },
       [']'] = { escape = true, close = false, pair = '[]' },
       ['}'] = { escape = true, close = false, pair = '{}' },
