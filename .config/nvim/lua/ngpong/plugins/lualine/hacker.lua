4128055f3c37d65497c1c85f976ffb8a238b9bbd
ngpong <pengpeng1997428@gmail.com>
2024-07-01 01:57:11 +0800

    ...

--- .config/nvim/lua/ngpong/plugins/lualine/hacker.lua
+++ .config/nvim/lua/ngpong/plugins/lualine/hacker.lua
@@ -6,8 +6,8 @@ local Lualine = Lazy.require('lualine')
 
 M.setup = function()
   local original_refresh = Lualine.refresh
-  Lualine.refresh = Bouncer.throttle_trailing(25, true, vim.schedule_wrap(function(opts)
-    original_refresh(opts)
+  Lualine.refresh = Bouncer.throttle_trailing(25, true, vim.schedule_wrap(function(...)
+    original_refresh(...)
   end));
 end
 
