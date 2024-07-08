a64259baa1db53bf8fd731bb09d97bdd3c2f5741
ngpong <pengpeng1997428@gmail.com>
2024-07-08 08:17:17 +0800

    ...

--- .config/nvim/lua/ngpong/plugins/whichkey/highlight.lua
+++ .config/nvim/lua/ngpong/plugins/whichkey/highlight.lua
@@ -3,8 +3,10 @@ local M = {}
 local colors = Plgs.colorscheme.colors
 
 M.setup = function()
-  vim.api.nvim_set_hl(0, 'WhichKey', { fg = colors.bright_green, italic = true, bold = true })
+  vim.api.nvim_set_hl(0, 'WhichKey', { fg = colors.bright_aqua, bold = true })
   vim.api.nvim_set_hl(0, 'WhichKeyFloat', { bg = colors.dark0_soft })
+  vim.api.nvim_set_hl(0, 'WhichKeyDesc', { fg = colors.light1, italic = true, bold = true })
+  vim.api.nvim_set_hl(0, 'WhichKeyGroup', { fg = colors.bright_red, bold = true })
   vim.api.nvim_set_hl(0, 'WhichKeyBorder', { bg = colors.dark0_soft, fg = colors.dark3 })
 end
 
